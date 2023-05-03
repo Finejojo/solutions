275b99abbbd5c5128bcfd4bb5fddc1316550ba8e
Finejojo <108051271+Finejojo@users.noreply.github.com>
2023-05-03 21:36:29 +0100

    updates

---
+++ primes/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primes
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int nums = 100;
+            for (int i = 0; i < nums; i++) 
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            
+        }
+    }
+    
+
+}
---
+++ primes/LeapYear.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+public class LeapYear
+{
+    public static void Main(string[] args)
+    {
+        int[] intArray = { 2, 7, 12 };
+        int multiply = intArray[0] * intArray[1];
+        int addition = intArray[0] + intArray[1];
+        bool isGreater = false;
+        if (multiply > intArray[2] || addition > intArray[2])
+        {
+            isGreater = true;
+        }
+        Console.WriteLine(isGreater);
+    }
+}
