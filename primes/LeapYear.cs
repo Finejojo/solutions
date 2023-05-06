fef33dc2e37b7d47c794df5b58c9f3b7af7761a3
Finejojo <108051271+Finejojo@users.noreply.github.com>
2023-05-06 15:46:29 +0100

    my new project

--- primes/LeapYear.cs
+++ primes/LeapYear.cs
@@ -2,18 +2,31 @@
 //Console.WriteLine("Hello, World!");
 using System;
 
-public class LeapYear
+namespace Leap
 {
-    public static void Main(string[] args)
+    public class LeapYear
     {
-        int[] intArray = { 2, 7, 12 };
-        int multiply = intArray[0] * intArray[1];
-        int addition = intArray[0] + intArray[1];
-        bool isGreater = false;
-        if (multiply > intArray[2] || addition > intArray[2])
+        public static void Main(string[] args)
         {
-            isGreater = true;
+            //int[] intArray = { 2, 7, 12 };
+            //int multiply = intArray[0] * intArray[1];
+            //int addition = intArray[0] + intArray[1];
+            //bool isGreater = false;
+            //if (multiply > intArray[2] || addition > intArray[2])
+            //{
+            //    isGreater = true;
+            //}
+            //Console.WriteLine(isGreater);
+            // new project
+            Console.WriteLine("what is the height of the building");
+            string input = Console.ReadLine();
+            double height = Convert.ToDouble(input);
+            if (height > 7) { Console.WriteLine("special fire safety measures required"); }
+            else { Console.WriteLine("No special safety measures required"); }
+
+            Console.ReadKey();
         }
-        Console.WriteLine(isGreater);
+        
     }
+   
 }
